f2bb76c23ee205381585522e4cd51a8da9e1357a
lihonggao <lihonggao@tencent.com>
2026-09-13 12:53:00 +0800

    orientation warping & stride warping

--- TypeScript/Project/Gameplay/BP_PlayerChar.ts
+++ TypeScript/Project/Gameplay/BP_PlayerChar.ts
@@ -14,6 +14,21 @@ class TS_BP_PlayerCharPlaceHold {
 Object.setPrototypeOf(TS_BP_PlayerCharPlaceHold.prototype, BP_PlayerChar.prototype);
 
 export class TS_BP_PlayerChar extends TS_BP_PlayerCharPlaceHold {
+    override SetAim(NewAim: boolean): void {
+        if (this.bAim == NewAim)
+            return;
+        this.bAim = NewAim;
+
+        const MoveData = NewAim ? this.PlayerWalkData : this.PlayerJogData;
+        const CharMovement = this.CharacterMovement;
+
+        CharMovement.MaxWalkSpeed = MoveData.MaxWalkSpeed;
+        CharMovement.MaxAcceleration = MoveData.MaxAcceleration;
+        CharMovement.BrakingDecelerationWalking = MoveData.BrakingDeceleration;
+        CharMovement.BrakingFrictionFactor = MoveData.BrakingFricationFactor;
+        CharMovement.BrakingFriction = MoveData.BrakingFriction;
+        CharMovement.bUseSeparateBrakingFriction = MoveData.UseSeperateBrakingFriction;
+    }
 }
 
 const Mixin_TS_BP_PlayerChar = blueprint.mixin(BP_PlayerChar, TS_BP_PlayerChar, {
