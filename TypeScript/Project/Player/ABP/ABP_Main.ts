4b670f5f0c476a121ab9760b961a5fa61200a7ab
lihonggao <lihonggao@tencent.com>
2026-09-13 11:37:43 +0800

    add cycle anim switch

--- TypeScript/Project/Player/ABP/ABP_Main.ts
+++ TypeScript/Project/Player/ABP/ABP_Main.ts
@@ -1,5 +1,5 @@
 import * as UE from "ue";
-import {blueprint} from "puerts";
+import {$ref, $unref, blueprint} from "puerts";
 
 const ucls = UE.Class.Load("/Game/Project/Player/ABP/ABP_Main.ABP_Main_C");
 
@@ -17,23 +17,60 @@ export class TS_ABP_Main extends TS_ABP_MainPlaceHold {
     override BlueprintInitializeAnimation(): void {
         console.log("TS_ABP_MainPlaceHold");
     }
-    
+
+    CalculateVelocityLocomotionDirection(): UE.Game.Project.Enums.E_VelocityLocomotionDirection.E_VelocityLocomotionDirection {
+        const locomotionDirection = UE.Game.Project.Enums.E_VelocityLocomotionDirection.E_VelocityLocomotionDirection;
+
+        if (this.Velocity2D.IsNearlyZero()) {
+            return locomotionDirection.None;
+        }
+
+        if (this.VelocityLocomotionAngle >= this.ForwardMin && this.VelocityLocomotionAngle <= this.ForwardMax) {
+            return locomotionDirection.Forward;
+        }
+
+        if (this.VelocityLocomotionAngle <= this.BackwardMin || this.VelocityLocomotionAngle >= this.BackwardMax) {
+            return locomotionDirection.Backward;
+        }
+
+        return this.VelocityLocomotionAngle < this.ForwardMin
+            ? locomotionDirection.Left
+            : locomotionDirection.Right;
+    }
+
     UpdateCharVars(): void {
-        const playerChar = this.TryGetPawnOwner() as UE.Game.Project.Gameplay.BP_PlayerChar.BP_PlayerChar_C;
+        const playerChar = this.TryGetPawnOwner();
         if (!playerChar) {
             return;
         }
-        
+
+        const playerCharInterface = playerChar as unknown as UE.Game.Project.Interface.BPI_PlayerChar.BPI_PlayerChar_C;
+        if (typeof playerCharInterface.GetbAim !== "function" || typeof playerCharInterface.GetEquipType !== "function") {
+            return;
+        }
+
         const velocity = playerChar.GetVelocity();
-        
+
         this.Velocity = velocity;
         this.Velocity2D.X = velocity.X;
         this.Velocity2D.Y = velocity.Y;
         this.Velocity2D.Z = 0;
         this.Velocity2DFloat = velocity.Size2D();
-        this.EquipType = playerChar.EquipType;
+        this.VelocityLocomotionAngle = UE.KismetAnimationLibrary.CalculateDirection(
+            this.Velocity2D,
+            playerChar.K2_GetActorRotation(),
+        );
+        this.VelocityLocomotionDir = this.CalculateVelocityLocomotionDirection();
+
+        const bAim = $ref(false);
+        playerCharInterface.GetbAim(bAim);
+        this.bAim = $unref(bAim);
+
+        const equipType = $ref<UE.Game.Project.Enums.E_PlayerEquipType.E_PlayerEquipType>();
+        playerCharInterface.GetEquipType(equipType);
+        this.EquipType = $unref(equipType);
     }
-    
+
     override BlueprintUpdateAnimation(DeltaTimeX: number): void {
         this.UpdateCharVars();
     }
