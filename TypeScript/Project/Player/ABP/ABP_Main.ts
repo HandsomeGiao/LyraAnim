06616ad92db07e4165577783a96d84efb3fecb8d
lihonggao <lihonggao@tencent.com>
2026-09-13 10:41:05 +0800

    add base cycle logic

--- TypeScript/Project/Player/ABP/ABP_Main.ts
+++ TypeScript/Project/Player/ABP/ABP_Main.ts
@@ -17,6 +17,26 @@ export class TS_ABP_Main extends TS_ABP_MainPlaceHold {
     override BlueprintInitializeAnimation(): void {
         console.log("TS_ABP_MainPlaceHold");
     }
+    
+    UpdateCharVars(): void {
+        const playerChar = this.TryGetPawnOwner() as UE.Game.Project.Gameplay.BP_PlayerChar.BP_PlayerChar_C;
+        if (!playerChar) {
+            return;
+        }
+        
+        const velocity = playerChar.GetVelocity();
+        
+        this.Velocity = velocity;
+        this.Velocity2D.X = velocity.X;
+        this.Velocity2D.Y = velocity.Y;
+        this.Velocity2D.Z = 0;
+        this.Velocity2DFloat = velocity.Size2D();
+        this.EquipType = playerChar.EquipType;
+    }
+    
+    override BlueprintUpdateAnimation(DeltaTimeX: number): void {
+        this.UpdateCharVars();
+    }
 }
 
 const Mixin_TS_ABP_Main = blueprint.mixin(ABP_Main, TS_ABP_Main, {
